82ba5c315351f2d978d82c677052ab1894d12839
Kate Brock <katebrock@Kates-MacBook-Pro.local>
2016-03-02 16:53:27 -0600

    working scroll top

---
+++ app.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+
+  $(window).scroll(function() {
+    console.log('scrolly scroll');
+    if($(window).scrollTop() >= 450) {
+      $('.socialLinks').css('background', 'pink');
+    } else {
+      $('.socialLinks').css('background', 'transparent');
+    }
+  });
+});
+
+
+
+
+
+// window.addEventListener("scroll", function(evt) {
+//    var pos_top = document.body.scrollTop;
+//    if(pos_top <= 50){
+//       $('#test').css('color','#fff');
+//       $('.logo').show()
+//    }
+//
+//    else if(pos_top > 50){
+//       $('#test').css('color','pink');
+//       $('.logo').hide()
+//    }
+// });
